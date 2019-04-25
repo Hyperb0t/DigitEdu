cf489c933046242d5d41a991b8b6a83c267f338b
Alexandr Ermakov <supertux038@gmail.com>
2019-04-25 18:54:18 +0300

    Added templates and static folder with some files

--- main/views.py
+++ main/views.py
@@ -4,30 +4,31 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import AnonymousUser
 from .models import SessionBeginDate
 
+
 def starter(request):
     return render(request, 'main/starter.html')
 
 
 def main(request):
-    return render(request, 'main/main.html', {"session" : SessionBeginDate.objects.all()[0]})
+    return render(request, 'main/main.html', {"session": SessionBeginDate.objects.all()[0]})
 
 
 def cabinet(request):
-    if(request.user.is_authenticated):
-        if(request.user.is_superuser):
+    if (request.user.is_authenticated):
+        if (request.user.is_superuser):
             return HttpResponseRedirect('/admin')
         else:
-            return render(request, 'main/cabinet.html', {"student":request.user.student})
+            return render(request, 'main/lk.html', {"student": request.user.student})
     else:
         return HttpResponseRedirect('/login')
 
 
 def loginUser(request):
-    if(request.method == 'POST'):
+    if (request.method == 'POST'):
         loginn = request.POST['login']
         password = request.POST['pswd']
         user = authenticate(request, username=loginn, password=password)
-        if(user is not None):
+        if (user is not None):
             login(request, user)
             return redirect('cabinet/')
         else:
