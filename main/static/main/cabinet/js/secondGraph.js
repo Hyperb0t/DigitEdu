08b9f21c437db0da6061ff0da5d5d7a3a09653e2
Hyperbot <vitalij-groshev@mail.ru>
2019-04-27 12:00:39 +0300

    bad graphs

--- main/static/main/cabinet/js/secondGraph.js
+++ main/static/main/cabinet/js/secondGraph.js
@@ -1,14 +1,16 @@
-var graphData;
-$.ajax({
-    method: "GET",
-    url: "/graphrestapi/1_1",
-    success: drawSecondGraph,
-    error: function (error_data) {
-        console.log("can't get data for second graph");
-        console.log(error_data);
-        drawGraph({});
-    }
-});
+function displayStudentSecondGraph(student) {
+    var graphData;
+    $.ajax({
+        method: "GET",
+        url: "/graphrestapi/" + student + "_1",
+        success: drawSecondGraph,
+        error: function (error_data) {
+            console.log("can't get data for second graph");
+            console.log(error_data);
+            drawGraph({});
+        }
+    });
+}
 
 function drawSecondGraph(data) {
     var ctx = document.getElementById("secondGraph");
